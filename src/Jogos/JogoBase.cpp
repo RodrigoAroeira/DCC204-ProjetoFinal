e1fc8b2350aebdf06b11ccc87db592ae92d48132
RodrigoAroeira <raroeirabr@gmail.com>
2025-01-17 13:27:13 -0300

    feat: Implementação do construtor e do método base `cadastrarJogador`

--- src/Jogos/JogoBase.cpp
+++ src/Jogos/JogoBase.cpp
@@ -1,1 +1,18 @@
 #include "JogoBase.hpp"
+
+#include <iostream>
+
+JogoBase::JogoBase(Leaderboard &leaderboard)
+    : mJogadoresPontuacao{}, mJogadorAtual{nullptr},
+      mLeaderboard(leaderboard) {};
+
+bool JogoBase::cadastrarJogador(const Jogador &jogador) {
+  if (mJogadoresPontuacao.find(jogador) != mJogadoresPontuacao.end()) {
+    std::cerr << "Jogador " << jogador.getNome() << " já está registrado"
+              << std::endl;
+    return false;
+  }
+
+  mJogadoresPontuacao[jogador] = 0;
+  return true;
+}
