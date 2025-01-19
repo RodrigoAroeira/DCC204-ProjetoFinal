6ebb5d79faf986f0788febfd9f91e8434328e10b
Julio César Barbosa Machado <164520972+Julio-Barbosa@users.noreply.github.com>
2025-01-19 17:45:28 -0300

    :tada: init: Primeira versão do Lig4.hpp

--- headers/Jogos/Lig4.hpp
+++ headers/Jogos/Lig4.hpp
@@ -1,5 +1,35 @@
 #pragma once
 
 #include "JogoBase.hpp"
+#include <vector>
+#include <map>
+#include <memory>
+#include <string>
+#include <iostream>
+#include <stdexcept>
 
-class Lig4 : public JogoBase {};
+class Lig4 : public JogoBase {
+public:
+    static const int ROWS = 6;
+    static const int COLS = 7;
+
+    Lig4();
+
+    bool cadastrarJogador(const Jogador &jogador) override;
+    void mudarJogadorAtual() override;
+    bool jogadaValida(int col) const;
+    void lerJogada() override;
+    bool verificarVitoria() const override;
+    void imprimirTabuleiro() const override;
+    void jogar() override;
+
+    friend std::ostream &operator<<(std::ostream &os, const Lig4 &j);
+
+private:
+    bool isTabuleiroCheio() const;
+
+private:
+    std::map<std::shared_ptr<Jogador>, float> mJogadoresPontuacao;
+    std::vector<std::vector<char>> mTabuleiro;
+    std::shared_ptr<Jogador> mJogadorAtual;
+};
