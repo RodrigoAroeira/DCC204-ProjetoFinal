9ab47e15e6e41bc7494e4291e0f99f6899fa1ab8
Julio César Barbosa Machado <164520972+Julio-Barbosa@users.noreply.github.com>
2025-01-21 21:27:08 -0300

    Update Lig4.hpp
    Remodelamento para condizer com a mudança na estrutura de integração

--- headers/Jogos/Lig4.hpp
+++ headers/Jogos/Lig4.hpp
@@ -1,35 +1,33 @@
 #pragma once
 
-#include "JogoBase.hpp"
-#include <vector>
 #include <map>
 #include <memory>
-#include <string>
+#include <vector>
 #include <iostream>
 #include <stdexcept>
+#include "JogoBase.hpp"
 
 class Lig4 : public JogoBase {
 public:
     static const int ROWS = 6;
     static const int COLS = 7;
 
-    Lig4();
+    Lig4(Leaderboard &leaderboard);
+    ~Lig4() override;
 
-    bool cadastrarJogador(const Jogador &jogador) override;
+    bool cadastrarJogador(std::shared_ptr<Jogador> jogador) override;
     void mudarJogadorAtual() override;
-    bool jogadaValida(int col) const;
+    bool jogadaValida(int col) const override;
     void lerJogada() override;
     bool verificarVitoria() const override;
     void imprimirTabuleiro() const override;
     void jogar() override;
 
-    friend std::ostream &operator<<(std::ostream &os, const Lig4 &j);
-
 private:
     bool isTabuleiroCheio() const;
 
 private:
-    std::map<std::shared_ptr<Jogador>, float> mJogadoresPontuacao;
+    std::map<std::shared_ptr<Jogador>, char> mJogadorTipo;
     std::vector<std::vector<char>> mTabuleiro;
     std::shared_ptr<Jogador> mJogadorAtual;
 };
